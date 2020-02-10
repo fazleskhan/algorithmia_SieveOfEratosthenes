420232dbdba5c5f2cc9ebd09fd4bda95fe4bd069
Fazle Khan <fazleskhan@users.noreply.github.com>
2020-02-10 16:07:08 -0500

    Adding Algorithmia files

---
+++ src/main/java/algorithmia/algorithmia_SieveOfEratosthenes/algorithmia_SieveOfEratosthenes.java
@@ -0,0 +1,13 @@
+package algorithmia.algorithmia_SieveOfEratosthenes;
+
+import com.algorithmia.*;
+import com.algorithmia.algo.*;
+import com.algorithmia.data.*;
+import com.google.gson.*;
+import java.util.*;
+
+public class algorithmia_SieveOfEratosthenes {
+    public String apply(String s) throws Exception {
+        return "Hello " + s;
+    }
+}
