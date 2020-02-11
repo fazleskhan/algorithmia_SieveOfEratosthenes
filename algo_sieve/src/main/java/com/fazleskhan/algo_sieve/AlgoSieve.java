67726037bc9a8e844e45094cf33389b3e5870954
Fazle Khan <fazleskhan@users.noreply.github.com>
2020-02-10 19:48:47 -0500

    fixed library issue sbt-sonatype and sbt-pgp do not have maven repos

--- algo_sieve/src/main/java/com/fazleskhan/algo_sieve/AlgoSieve.java
+++ algo_sieve/src/main/java/com/fazleskhan/algo_sieve/AlgoSieve.java
@@ -1,5 +1,11 @@
 package com.fazleskhan.algo_sieve;
 
+import com.algorithmia.*;
+import com.algorithmia.algo.*;
+import com.algorithmia.data.*;
+import com.google.gson.*;
+import java.util.*;
+
 public class AlgoSieve {
 
        public String apply(String s) throws Exception {
---
+++ algo_sieve/src/test/java/com/fazleskhan/algo_sieve/AlgoSieveTests.java
@@ -0,0 +1,4 @@
+package com.fazleskhan.algo_sieve;
+
+public class AlgoSieveTests {
+}
