ba88efa9f66e9a8192646e6bb55295701a9e79e8
Fazle Khan <fazleskhan@users.noreply.github.com>
2020-02-19 15:00:24 -0500

    added unit test for 100 as the last number

--- algo_sieve/src/test/java/com/fazleskhan/algo_sieve/AlgoSieveTests.java
+++ algo_sieve/src/test/java/com/fazleskhan/algo_sieve/AlgoSieveTests.java
@@ -16,4 +16,16 @@ public class AlgoSieveTests {
                 "Sieve of Eratosthenes \\(last number 0\\): messages - \\[Start calculating prime numbers between 1 and 0, Start Sieve, Number of primes found 0, Sieve took \\d* ms\\] primes - \\[\\]"
         ));
     }
+
+    //Sieve of Eratosthenes (last number 100): messages - [Start calculating prime numbers between 1 and 100, Start Sieve, Number of primes found 25, Sieve took 43 ms] primes - [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    @Test
+    public void OneHundredLastNumber(){
+        AlgoSieve target = new AlgoSieve();
+
+        String result = target.apply("100");
+        assertThat(result, matchesPattern(
+                "Sieve of Eratosthenes \\(last number 100\\): messages - \\[Start calculating prime numbers between 1 and 100, Start Sieve, Number of primes found 25, Sieve took \\d* ms\\] primes - \\[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97\\]"
+        ));
+    }
+
 }
