027237b80fd1870323edffe4491d25360842131a
Fazle Khan <fazleskhan@users.noreply.github.com>
2020-02-19 14:22:07 -0500

    added failing 100 lastnumber test

--- algo_sieve/src/test/java/com/fazleskhan/algo_sieve/AlgoSieveTests.java
+++ algo_sieve/src/test/java/com/fazleskhan/algo_sieve/AlgoSieveTests.java
@@ -16,4 +16,15 @@ public class AlgoSieveTests {
                 "Sieve of Eratosthenes (last number 0): messages - [Start calculating prime numbers between 1 and 0, Start Sieve, Number of primes found 0, Sieve took 44 ms] primes - []"
         ));
     }
+
+    @Test
+    public void LastNumberOneHundred(){
+        AlgoSieve target = new AlgoSieve();
+
+        String result = target.apply("100");
+        assertThat(result,is(
+                "Sieve of Eratosthenes (last number 100): messages - [Start calculating prime numbers between 1 and 100, Start Sieve, Number of primes found 25, Sieve took 49 ms] primes - [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]"
+        ));
+
+    }
 }
